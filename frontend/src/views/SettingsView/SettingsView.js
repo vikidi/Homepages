a75ee8468df8f7bf0a19e2eaad16d74ddbc3d438
Ville Saarinen <ville.saarinen9@gmail.com>
2020-12-20 18:43:33 +0200

    Initial theme changer

--- frontend/src/views/SettingsView/SettingsView.js
+++ frontend/src/views/SettingsView/SettingsView.js
@@ -1,6 +1,8 @@
 import React from 'react'
 //import { useTranslation } from 'react-i18next'
-//import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux'
+
+import { setTheme } from '../../reducers/themeReducer'
 
 // Components
 import Grid from '@material-ui/core/Grid'
@@ -8,14 +10,36 @@ import Grid from '@material-ui/core/Grid'
 import GridContainer from '../../components/GridContainer/GridContainer'
 import BackgroundPaper from '../../components/BackgroundPaper/BackgroundPaper'
 
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
+
+import { availableThemes } from '../../utils/themeService'
+
 const SettingsView = () => {
   //const t = useTranslation()
+  const d = useDispatch()
+
+  const theme = useSelector(store => store.theme)
+
+  const handleChange = (event) => {
+    d(setTheme({ name: event.target.value }))
+  }
 
   return (
     <GridContainer>
       <Grid item xs={9}>
         <BackgroundPaper fullHeight>
-          <p>TEST</p>
+          <FormControl>
+            <InputLabel>Theme</InputLabel>
+            <Select
+              value={theme.name}
+              onChange={handleChange}
+            >
+              {availableThemes.map(curTheme => <MenuItem key={curTheme.name} value={curTheme.name}>{curTheme.name}</MenuItem>)}
+            </Select>
+          </FormControl>
         </BackgroundPaper>
       </Grid>
     </GridContainer>

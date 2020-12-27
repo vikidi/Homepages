a0051169c34d33a2319042f43731d17221c99ea6
Ville Saarinen <ville.saarinen9@gmail.com>
2020-12-27 20:34:32 +0200

    Trasform profile pic to webp-format

--- frontend/src/views/MainView/MainView.js
+++ frontend/src/views/MainView/MainView.js
@@ -63,7 +63,7 @@ const MainView = () => {
           <GridContainer horizontal='center' direction='column'>
 
             <Grid item className={classes.profilePic}>
-              <CustomImage round path='/img/ProfilePicSquareCartoon.png' alt='Profile picture of me' />
+              <CustomImage round path='/img/ProfilePicSquareCartoon.webp' alt='Profile picture of me' />
             </Grid>
 
             <Grid item>

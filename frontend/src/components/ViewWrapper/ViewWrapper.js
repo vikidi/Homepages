e3dbcc6aa611c5d775e8c2273c4ff7029c633670
Restyled.io <commits@restyled.io>
2021-03-10 17:34:39 +0000

    Restyled by prettier

--- frontend/src/components/ViewWrapper/ViewWrapper.js
+++ frontend/src/components/ViewWrapper/ViewWrapper.js
@@ -1,43 +1,42 @@
 // Material UI Components
-import Container from '@material-ui/core/Container'
-import {makeStyles} from '@material-ui/styles'
-import PropTypes from 'prop-types'
-import React from 'react'
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/styles";
+import PropTypes from "prop-types";
+import React from "react";
 
-import Footer from '../Footer/Footer'
+import Footer from "../Footer/Footer";
 // Own Components
-import Navigation from '../Navigation/Navigation'
+import Navigation from "../Navigation/Navigation";
 
-const useStyles = makeStyles(
-    () => ({
-      root : {marginTop : 'auto', marginBottom : 'auto', height : '100%'},
-    }))
+const useStyles = makeStyles(() => ({
+  root: { marginTop: "auto", marginBottom: "auto", height: "100%" },
+}));
 
-const ViewWrapper = ({header, footer, fullMain, children}) => {
-  const classes = useStyles()
+const ViewWrapper = ({ header, footer, fullMain, children }) => {
+  const classes = useStyles();
 
   return (
     <>
       {header && <Navigation />}
-      <Container className={fullMain ? classes.root : null} component='main'>
+      <Container className={fullMain ? classes.root : null} component="main">
         {children}
       </Container>
-      {footer && <Footer />
-} < />
-  )
-}
+      {footer && <Footer />}{" "}
+    </>
+  );
+};
 
 ViewWrapper.propTypes = {
   children: PropTypes.node,
   header: PropTypes.bool,
   footer: PropTypes.bool,
-  fullMain: PropTypes.bool
-}
+  fullMain: PropTypes.bool,
+};
 
 ViewWrapper.defaultProps = {
   header: true,
   footer: true,
-  fullMain: false
-}
+  fullMain: false,
+};
 
-export default ViewWrapper
+export default ViewWrapper;
--- frontend/src/views/SettingsView/SettingsView.js
+++ frontend/src/views/SettingsView/SettingsView.js
@@ -1,85 +1,99 @@
-import Container from '@material-ui/core/Container'
-import Divider from '@material-ui/core/Divider'
+import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
 // Material UI Components
-import Grid from '@material-ui/core/Grid'
-import MenuItem from '@material-ui/core/MenuItem'
-import Typography from '@material-ui/core/Typography'
+import Grid from "@material-ui/core/Grid";
+import MenuItem from "@material-ui/core/MenuItem";
+import Typography from "@material-ui/core/Typography";
 // Material UI Icons
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline'
-import {makeStyles, useTheme} from '@material-ui/styles'
-import React from 'react'
-import {useTranslation} from 'react-i18next'
-import {useDispatch, useSelector} from 'react-redux'
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import { makeStyles, useTheme } from "@material-ui/styles";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
 
-import BackgroundPaper from '../../components/BackgroundPaper/BackgroundPaper'
+import BackgroundPaper from "../../components/BackgroundPaper/BackgroundPaper";
 // Custom Components
-import GridContainer from '../../components/GridContainer/GridContainer'
-import Selector from '../../components/Selector/Selector'
-import ViewWrapper from '../../components/ViewWrapper/ViewWrapper'
-import {setLanguage} from '../../reducers/languageReducer'
+import GridContainer from "../../components/GridContainer/GridContainer";
+import Selector from "../../components/Selector/Selector";
+import ViewWrapper from "../../components/ViewWrapper/ViewWrapper";
+import { setLanguage } from "../../reducers/languageReducer";
 // Reducers
-import {setTheme} from '../../reducers/themeReducer'
-import {getByName, languages} from '../../utils/languageUtil'
+import { setTheme } from "../../reducers/themeReducer";
+import { getByName, languages } from "../../utils/languageUtil";
 // Services
-import {availableThemes, getThemeByName} from '../../utils/themeUtil'
+import { availableThemes, getThemeByName } from "../../utils/themeUtil";
 
-const useStyles = makeStyles(theme => ({
-                               root : {marginRight : theme.spacing(1)},
-                             }))
+const useStyles = makeStyles((theme) => ({
+  root: { marginRight: theme.spacing(1) },
+}));
 
-const Autosave =
-    () => {
-      const classes = useStyles()
-      const theme = useTheme()
-      const {t} = useTranslation()
-
-      return (
-          <GridContainer vertical = 'center' horizontal = 'flex-end'>
-          <CheckCircleOutlineIcon className = {classes.root} htmlColor = {
-            theme.palette.success.main
-          } />
-      {t('SettingsView.Autosave.label')}
-    </GridContainer>)
-    }
-
-const SettingsView = () => {
-  const {t, i18n} = useTranslation()
-  const d = useDispatch()
-
-  const theme = useSelector(store => store.theme)
-  const language = useSelector(store => store.language)
-
-  const handleThemeChange =
-      event => { d(setTheme(getThemeByName(event.target.value))) }
-
-  const handleLanguageChange =
-      event => {
-        const langObj = getByName(event.target.value)
-        i18n.changeLanguage(langObj['short'])
-        d(setLanguage(langObj))
-      }
+const Autosave = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const { t } = useTranslation();
 
   return (
-      <ViewWrapper header footer fullMain><GridContainer><Grid item md = {9}>
-      <BackgroundPaper><GridContainer direction = 'column' vertical = 'stretch'>
+    <GridContainer vertical="center" horizontal="flex-end">
+      <CheckCircleOutlineIcon
+        className={classes.root}
+        htmlColor={theme.palette.success.main}
+      />
+      {t("SettingsView.Autosave.label")}
+    </GridContainer>
+  );
+};
 
-      <Typography variant = "h3" gutterBottom>{t('SettingsView.title')}<
-          /Typography>
-              <Container maxWidth='sm'>
-                <Typography variant="subtitle1" display="block" gutterBottom>{t('SettingsView.description')}</Typography>
-      </Container>
+const SettingsView = () => {
+  const { t, i18n } = useTranslation();
+  const d = useDispatch();
 
+  const theme = useSelector((store) => store.theme);
+  const language = useSelector((store) => store.language);
+
+  const handleThemeChange = (event) => {
+    d(setTheme(getThemeByName(event.target.value)));
+  };
+
+  const handleLanguageChange = (event) => {
+    const langObj = getByName(event.target.value);
+    i18n.changeLanguage(langObj["short"]);
+    d(setLanguage(langObj));
+  };
+
+  return (
+    <ViewWrapper header footer fullMain>
+      <GridContainer>
+        <Grid item md={9}>
+          <BackgroundPaper>
+            <GridContainer direction="column" vertical="stretch">
+              <Typography variant="h3" gutterBottom>
+                {t("SettingsView.title")}
+              </Typography>
+              <Container maxWidth="sm">
+                <Typography variant="subtitle1" display="block" gutterBottom>
+                  {t("SettingsView.description")}
+                </Typography>
+              </Container>
               <Selector
-                label={t('ThemeSelection.label')}
+                label={t("ThemeSelection.label")}
                 value={theme.name}
                 handleChange={handleThemeChange}
-                menuItems={availableThemes.map(curTheme => <MenuItem key={curTheme.name} value={curTheme.name}>{t(`ThemeSelection.ThemeNames.${curTheme.name}`)}</MenuItem>)
-} />
-              <Divider / > < Selector
-                label={t('LanguageSelection.label')}
+                menuItems={availableThemes.map((curTheme) => (
+                  <MenuItem key={curTheme.name} value={curTheme.name}>
+                    {t(`ThemeSelection.ThemeNames.${curTheme.name}`)}
+                  </MenuItem>
+                ))}
+              />
+              <Divider />{" "}
+              <Selector
+                label={t("LanguageSelection.label")}
                 value={language.name}
                 handleChange={handleLanguageChange}
-                menuItems={languages.map(lang => <MenuItem key={lang.short} value={lang.name}>{lang.name}</MenuItem>)}
+                menuItems={languages.map((lang) => (
+                  <MenuItem key={lang.short} value={lang.name}>
+                    {lang.name}
+                  </MenuItem>
+                ))}
               />
               <Autosave />
             </GridContainer>
@@ -87,7 +101,7 @@ const SettingsView = () => {
         </Grid>
       </GridContainer>
     </ViewWrapper>
-  )
-}
+  );
+};
 
-export default SettingsView
+export default SettingsView;
